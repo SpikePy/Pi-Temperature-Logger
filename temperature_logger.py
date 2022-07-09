bf40ff3fce34313abdcc65491d4976c6e8df2b9a
Rene Hoffmann <hoffelmann@gmail.com>
2022-07-09 15:42:48 +0200

    add comments to code

--- temperature_logger.py
+++ temperature_logger.py
@@ -108,17 +108,23 @@ def get_sensor_data():
 
 def insert_text_to_line_in_file(insert_text, insert_marker, file_path):
     with open(file_path, "r") as file:
+        # store each line of the file as an element of a list
         content = file.readlines()
 
+        # alternative approach
         # index_marker = content.index(insert_marker, 10, 50)
         # content.insert(index_marker + 1, text)
         # content = "".join(content)
 
+        # go through lines by index
         for i in range(len(content)):
+
+            # if the current line containins the defined marker insert text and exit loop
             if insert_marker in content[i]:
                 content.insert(i + 1, insert_text)
                 break
 
+        # combine list of lines to one multiline string
         content = "".join(content)
 
     with open(file_path, "w") as file:
