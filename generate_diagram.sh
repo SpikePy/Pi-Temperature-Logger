9cee396da5a45677ddcfc6dcddc1c9683148bc43
Rene Hoffmann <hoffelmann@gmail.com>
2022-08-18 18:14:05 +0200

    improve diagram

--- generate_diagram.sh
+++ generate_diagram.sh
@@ -10,4 +10,40 @@ cat data_all.csv   | head -${meassurements_per_month} > data_month.csv
 cat data_month.csv | head -${meassurements_per_week}  > data_week.csv
 cat data_week.csv  | head -${meassurements_per_day}   > data_day.csv
 
-gnuplot -p diagram.gnuplot
+gnuplot << EOF
+set title "Temperature Log" 
+
+# disable legend
+set nokey
+
+set datafile separator ','
+
+set grid ls 1 lw .25 lc black xtics ytics
+
+set ylabel "Temperature [°C]"
+set yrange [10:35]
+set ytics 0,2,35
+
+set xlabel "Date"
+set timefmt "%y-%m-%d_%H:%M"  # specify our time string format
+set format x "%y-%m-%d" # otherwise it will show only MM:SS
+set xdata time # tells gnuplot the x axis is time data
+set xrange [* : *] reverse 
+set xtics rotate by -45
+
+set term svg size 2000,700
+
+set output "diagram_all.svg"
+plot "data_all.csv" using 1:2 with lines lw 2
+
+set output "diagram_month.svg"
+plot "data_month.csv" using 1:2 with lines lw 2
+
+set format x "%y-%m-%d %A"
+set output "diagram_week.svg"
+plot "data_week.csv" using 1:2 with lines lw 2
+
+set format x "%y-%m-%d %A %H:%M"
+set output "diagram_day.svg"
+plot "data_day.csv" using 1:2 with lines lw 2
+EOF
