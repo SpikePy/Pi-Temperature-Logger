0842d67b97a167b003d6e12d0235d7dc3bc7a976
Rene Hoffmann <hoffelmann@gmail.com>
2022-09-26 22:24:55 +0200

    improve diagram generation script

--- generate_diagram.sh
+++ generate_diagram.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env sh
 
-meassurements_per_hour=12 # depends on how the cronjob is configured
-meassurements_per_day=$(( meassurements_per_hour * 24 ))
-meassurements_per_week=$(( meassurements_per_day * 7 ))
-meassurements_per_month=$(( meassurements_per_day * 30 ))
-
+# transform data from JavaScript Object Literale to CSV
 tail -n +2 data.js | tr -d '"[] ;' | cut -d',' -f1,2  > data_all.csv
-cat data_all.csv   | head -${meassurements_per_month} > data_month.csv
-cat data_month.csv | head -${meassurements_per_week}  > data_week.csv
-cat data_week.csv  | head -${meassurements_per_day}   > data_day.csv
+cat data_all.csv   | sed -n "1,/$(date --date='month ago' +'%y-%m-%d_%H:00')/p" > data_month.csv
+cat data_month.csv | sed -n "1,/$(date --date='week ago'  +'%y-%m-%d_%H:00')/p" > data_week.csv
+cat data_week.csv  | sed -n "1,/$(date --date='yesterday' +'%y-%m-%d_%H:00')/p" > data_day.csv
 
 gnuplot << 'EOF'
 set title "Temperature Log" 
